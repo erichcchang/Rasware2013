5e49ab9d68ff03e662e45b11b58a3ccce56fbe8f
Sam Moser <smoser7@gmail.com>
2014-10-24 20:06:18 -0500

    added wall following and corner turning that works after a 30 point turn that is kinda slow

--- RASproject/Main.c
+++ RASproject/Main.c
@@ -6,22 +6,49 @@
 
 tADC *adc[2];
 tMotor *servomotor[2];
+float frontSensor;
+float rightSensor;
+int i;
+
+
+
 void initIRSensor(void) {
     adc[0] = InitializeADC(PIN_D0);
     adc[1] = InitializeADC(PIN_D1);
 }
 void initMotor(void) {
-		servomotor[0] = InitializeServoMotor(PIN_B6,true);
-		servomotor[1] = InitializeServoMotor(PIN_B7,true);
+		servomotor[0] = InitializeServoMotor(PIN_B6,false);			//left motor
+		servomotor[1] = InitializeServoMotor(PIN_B7,true);			//right motor
 }
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
-		initIRSensor();  	
+		initIRSensor(); 
+		initMotor();
     while (1) {
-        // Runtime code can go here
-       float ADCValue0 = ADCRead(adc[0]);
-			 float ADCValue1 = ADCRead(adc[1]);
-			
+      // Runtime code can go here
+      frontSensor = ADCRead(adc[0])*1000;
+			rightSensor = ADCRead(adc[1])*1000;
+			if(frontSensor > 600){
+				SetMotor(servomotor[0], -1);
+				SetMotor(servomotor[1], -.5);
+				i = 1000000;
+				while(i){
+					i -= 1;
+				}
+				
+				
+				SetMotor(servomotor[0], 0);
+				SetMotor(servomotor[1], 1);
+			}
+			else if(rightSensor > 500){
+				SetMotor(servomotor[0], -1);
+				SetMotor(servomotor[1], 0);
+			}
+			else{
+				SetMotor(servomotor[0], 1);
+				SetMotor(servomotor[1], .6);
+			}
+
     }
 }
